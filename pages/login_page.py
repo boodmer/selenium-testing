4d4ef81c8a0a775e7e08fdf1a0d672b8faebdea4
HieuGaLam <Hieu1#>
2025-11-17 22:57:37 +0700

    login

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
 
-    def __init__(self, driver, base_url = "https://the-internet.herokuapp.com"):
+    def __init__(self, driver, base_url = "http://127.0.0.1:8000"):
         self.driver = driver
         self.base_url = base_url
 
@@ -12,11 +12,16 @@ class LoginPage:
         self.driver.get(self.base_url + "/login")
 
     def login(self, username, password):
-        self.driver.find_element(By.ID, "username").send_keys(username)
+        self.driver.find_element(By.ID, "email").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-    def get_message(self):
+    def get_flash_message(self):
         return self.driver.wait.until(
-            EC.visibility_of_element_located((By.ID, "flash"))
+            EC.visibility_of_element_located((By.ID, ".fl-message"))
         ).text
+    
+    def get_error_message(self):
+         return self.driver.wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, ".error-message"))
+        ).text     
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,18 @@
 from pages.login_page import LoginPage
 from selenium.webdriver.chrome.webdriver import WebDriver
 
+# def test_login_success(driver: WebDriver):
+#     login = LoginPage(driver)
+#     login.open()
+#     login.login("tomsmith", "SuperSecretPassword!")
+    
+#     flash_text = login.get_message()
+#     assert "You logged into a secure area!" in flash_text
+
 def test_login_success(driver: WebDriver):
     login = LoginPage(driver)
     login.open()
-    login.login("tomsmith", "SuperSecretPassword!")
+    login.login("", "SuperSecretPassword!")
     
-    flash_text = login.get_message()
-    assert "You logged into a secure area!" in flash_text
+    flash_text = login.get_error_message()
+    assert "Vui lòng nhập email" in flash_text
